4f17408b70a75289bdd31f9ec86acc027cb993e0
krism <kris.mendoza1234@yahoo.com>
2025-04-07 09:04:01 -0700

    updoot list

--- tessy6.py
+++ tessy6.py
@@ -5,12 +5,22 @@ air_enemy = ["UAV", "BOGEY", "BANDIT", "BANZAI",
     "BREAK (DIRECTION)", "BREAKAWAY", "BINGO", "BREVITY", "BRUISER", "BUGOUT", "BUSTER", "MIG-21", "MIG-15", "MIG-17", "MIG-19", "MIG-23", "MIG-25", "MIG-29", "MIG-31",
     "SU-7", "SU-9", "SU-11", "SU-15", "SU-17", "SU-20", "SU-22", "SU-24", "SU-25",
     "SU-27", "SU-30", "SU-33", "SU-35", "SU-57", "YAK-9", "YAK-23", "YAK-25", "YAK-28",
-    "YAK-38", "YAK-41", "J-16", "J-8"
+    "YAK-38", "YAK-41", "J-16", "J-50", "J-36", "J-35", "J-20", "J-15", "J-16", "J-10B", "J-11B", "J-10", "FC-1", 
+    "JH-7", "Su-30MK2", "Su-30MKK", "Su-35S", "J-11D", "J-13", "J-12", "J-11", "J-9", 
+    "J-8", "J-7", "J-6", "J-5", "J-2", "MiG-9", "Su-27", "Q-5", "Q-6", "J-16D", "J-15D", 
+    "H-20", "H-8", "H-7", "H-6", "H-5", "Tu-14", "Tu-2", "KJ-600", "KJ-500", "KJ-2000", 
+    "Y-9JZ ELINT", "Y-8CB", "Y-8DZ", "Y-8G", "Y-8GX3", "Y-8JB", "Y-8T", "Y-8W", "Y-8EW", 
+    "ZDK03", "Y-8J", "KJ-200", "Y-7", "KJ-1 AEWC", "An-30", "CIOMP", 
+    "YY-20", "HY-6", "Il-78", "JZ-8", "JZ-7", "MIG-29", "MIG-31", "MIG-35", "SU-27", "SU-30", "SU-34", "SU-35", "SU-57", 
+    "SU-24", "SU-25", "MI-8", "MI-24", "MI-28", "KA-52", "KA-60", "ANSAT", 
+    "PANTHER", "IL-76", "IL-78", "IL-112", "IL-214", "AN-26", "AN-72", 
+    "BE-200", "TU-22M", "TU-160", "TU-95", "B-2", "S-400", "T-50", "T-14", "AWACS"
 ]
 
 surface = ["DESTROYER", "DD", "FRIGATE", "FF", "CRUISER", "CC", "SUBMARINE", "SUB", "CARRIER", "CV", "BANZAI", "BRACKET", "BUMP/BUMP-UP", "BURN GLINT", "BUSTER", "BLANK", 
     "BULLDOG", "BREVITY", "BANDIT", "BOGEY", "BASSETT", "BRUISER", "BULLDOG", "COBRA", "COWBOYS", 
-    "CYCLOPS", "DEADEYE", "JASSM", "ATTACK", "AAV", "DDG"
+    "CYCLOPS", "DEADEYE", "JASSM", "ATTACK", "AAV", "DDG", "CVN", "DDG", "CG", "LHD", "LPD", "LCS", "SSN", "SSBN", 
+    "LHA", "T-AKE", "AOR", "LST", "T-AO"
 ]
 
 intel = ["RADIO", "EMISSION", "EMISSIONS", "BLUR", "AUTOCAT", "BEAM RIDER"]
@@ -60,7 +70,7 @@ def extracted_chat(message):
     first_parenthesis_pos = message_upper.find(')') 
     second_parenthesis_pos = message_upper.find(')', first_parenthesis_pos + 1)
     
-    # Extract the text after second '('
+    # Extract the text after second ')'
     if second_parenthesis_pos != -1:
         text_second_parenthesis = message_upper[second_parenthesis_pos + 1:]  # Everything after the second ')'
         
