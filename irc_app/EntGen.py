8537b3ed8c452d0742428872cd2c5862a48d68db
cstone157 <culver.stone57@gmail.com>
2025-07-25 20:11:52 +0000

    Added .gitIgnore.  Cleanedup o action_prompt.  Added test case.  Moved constants into json files.

--- irc_app/EntGen.py
+++ irc_app/EntGen.py
@@ -1,79 +1,16 @@
 import re
+import os
+import json
 
-air_enemy = ["UAV", "BOGEY", "BANDIT", "BANZAI",
-    "BANDIT", "BOGEY", "BOGEY DOPE", "BLOW THROUGH", "BLOODHOUND", "BRACKET", 
-    "BREAK (DIRECTION)", "BREAKAWAY", "BINGO", "BREVITY", "BRUISER", "BUGOUT", "BUSTER", "MIG-21", "MIG-15", "MIG-17", "MIG-19", "MIG-23", "MIG-25", "MIG-29", "MIG-31",
-    "SU-7", "SU-9", "SU-11", "SU-15", "SU-17", "SU-20", "SU-22", "SU-24", "SU-25",
-    "SU-27", "SU-30", "SU-33", "SU-35", "SU-57", "YAK-9", "YAK-23", "YAK-25", "YAK-28",
-    "YAK-38", "YAK-41", "J-16", "J-50", "J-36", "J-35", "J-20", "J-15", "J-16", "J-10B", "J-11B", "J-10", "FC-1", 
-    "JH-7", "Su-30MK2", "Su-30MKK", "Su-35S", "J-11D", "J-13", "J-12", "J-11", "J-9", 
-    "J-8", "J-7", "J-6", "J-5", "J-2", "MiG-9", "Su-27", "Q-5", "Q-6", "J-16D", "J-15D", 
-    "H-20", "H-8", "H-7", "H-6", "H-5", "Tu-14", "Tu-2", "KJ-600", "KJ-500", "KJ-2000", 
-    "Y-9JZ ELINT", "Y-8CB", "Y-8DZ", "Y-8G", "Y-8GX3", "Y-8JB", "Y-8T", "Y-8W", "Y-8EW", 
-    "ZDK03", "Y-8J", "KJ-200", "Y-7", "KJ-1 AEWC", "An-30", "CIOMP", 
-    "YY-20", "HY-6", "Il-78", "JZ-8", "JZ-7", "MIG-29", "MIG-31", "MIG-35", "SU-27", "SU-30", "SU-34", "SU-35", "SU-57", 
-    "SU-24", "SU-25", "MI-8", "MI-24", "MI-28", "KA-52", "KA-60", "ANSAT", 
-    "PANTHER", "IL-76", "IL-78", "IL-112", "IL-214", "AN-26", "AN-72", 
-    "BE-200", "TU-22M", "TU-160", "TU-95", "B-2", "S-400", "T-50", "T-14", 
-    "AWACS", "AIRCRAFT", "HELICOPTER", "HELO", "BOMBER", "BOMBERS", "FIGHTER", "FIGHTERS", "A/C", "AIRCRAFTS",
-    "MIG21", "MIG15", "MIG17", "MIG19", "MIG23", "MIG25", "MIG29", "MIG31",
-    "SU7", "SU9", "SU11", "SU15", "SU17", "SU20", "SU22", "SU24", "SU25",
-    "SU27", "SU30", "SU33", "SU35", "SU57", "YAK9", "YAK23", "YAK25", "YAK28",
-    "YAK38", "YAK41", "J16", "J50", "J36", "J35", "J20", "J15", "J16", "J10B", "J11B", "J10", "FC1", 
-    "JH7", "Su30MK2", "Su30MKK", "Su35S", "J11D", "J13", "J12", "J11", "J9", 
-    "J8", "J7", "J6", "J5", "J2", "MiG9", "Su27", "Q5", "Q6", "J16D", "J15D", 
-    "H20", "H8", "H7", "H6", "H5", "Tu14", "Tu2", "KJ600", "KJ500", "KJ2000", 
-    "Y9JZ ELINT", "Y8CB", "Y8DZ", "Y8G", "Y8GX3", "Y8JB", "Y8T", "Y8W", "Y8EW", 
-    "ZDK03", "Y8J", "KJ200", "Y7", "KJ1 AEWC", "An30",
-    "YY20", "HY6", "Il78", "JZ8", "JZ7", "MIG29", "MIG31", "MIG35", "SU27", "SU30", "SU34", "SU35", "SU57", 
-    "SU24", "SU25", "MI8", "MI24", "MI28", "KA52", "KA60", "IL76", "IL78", "IL112", "IL214", "AN26", "AN72", 
-    "BE200", "TU22M", "TU160", "TU95", "B2", "S400", "T50", "T14"
-]
 
-surface = ["DESTROYER", "DD", "FRIGATE", "FF", "CRUISER", "CC", "SUBMARINE", "SUB", 
-    "CARRIER", "CV", "BANZAI", "BRACKET", "BUMP/BUMP-UP", "BURN-GLINT", "BUSTER", 
-    "BLANK", "BULLDOG", "BREVITY", "BANDIT", "BOGEY", "BASSETT", "BRUISER", "COBRA", 
-    "COWBOYS", "CYCLOPS", "DEADEYE", "JASSM", "ATTACK", "AAV", "DDG", "CVN", "CG", 
-    "LHD", "LPD", "LCS", "SSN", "SSBN", "LHA", "T-AKE", "AOR", "LST", "T-AO", "FFG", 
-    "RADAR", "VESSEL", "SHIP", "BB", "CA", "SSGN", "LSD", "AO", "AE", "AK", "AR", "PC", 
-    "PY", "MCM", "MSC", "AIRCRAFT-CARRIER", "NUCLEAR-POWERED-AIRCRAFT-CARRIER", "BATTLESHIP", 
-    "HEAVY-CRUISER", "GUIDED-MISSILE-CRUISER", "DESTROYER", "GUIDED-MISSILE-DESTROYER", 
-    "FRIGATE", "GUIDED-MISSILE-FRIGATE", "SUBMARINE-(DIESEL-ELECTRIC)", 
-    "NUCLEAR-POWERED-SUBMARINE-(ATTACK)", "NUCLEAR-POWERED-BALLISTIC-MISSILE-SUBMARINE", 
-    "NUCLEAR-POWERED-GUIDED-MISSILE-SUBMARINE", "AMPHIBIOUS-ASSAULT-SHIP", 
-    "AMPHIBIOUS-ASSAULT-SHIP-(DIFFERENT-CONFIGURATION)", "AMPHIBIOUS-TRANSPORT-DOCK", 
-    "LANDING-SHIP-TANK", "DOCK-LANDING-SHIP", "FLEET-OILER", "AMMUNITION-SHIP", 
-    "CARGO-SHIP", "REPAIR-SHIP", "PATROL-CRAFT", "PATROL-YACHT", 
-    "MINE-COUNTERMEASURES-SHIP", "MINESWEEPER-COMMAND-SHIP"
+# To ensure that we load the words file from the directory of the script we have to do these extra steps
+script_dir = os.path.dirname(__file__)
+with open(os.path.join(script_dir, 'words.json'), 'r') as file:
+    words = json.load(file)
+    ACTIONS = words["ACTIONS"]
+    WORDS = words["WORDS"]
+    
 
-]
-Incoming = ["MISSILE", "ROCKET", "TORPEDO", "BOMB", "TORP", "CRUISE", "BALLISTIC","LAUNCHED", "LAUNCH", "FIRE", "ENGAGED",
-             "ANTI-SHIP MISSILE", "ASW", "AAM", "AGM", "A/S", "A/G", "A/A", "A/T", "A/D", "A/P", "A/L",
-               "SURFACE-TO-AIR", "CRUISE-MISSILE","CRUISE",
-    "BALLISTIC-MISSILE",
-    "ANTI-SHIP-MISSILE",
-    "ANTI-AIR-MISSILE",
-    "AIR-TO-GROUND-MISSILE",
-    "SURFACE-TO-AIR-MISSILE",
-    "SURFACE-TO-SURFACE-MISSILE",
-    "LAND-ATTACK-CRUISE-MISSILE",
-    "SUBMARINE-LAUNCHED-CRUISE-MISSILE",
-    "SUBMARINE-LAUNCHED-BALLISTIC-MISSILE",
-    "SUBMARINE-LAUNCHED-ANTI-SHIP-MISSILE",
-    "SUBMARINE-LAUNCHED-LAND-ATTACK-CRUISE-MISSILE"
-    "SUBMARINE-LAUNCHED-ANTI-AIR-MISSILE"
-    ]
-
-intel = ["RADIO", "EMISSION", "EMISSIONS", "BLUR", "AUTOCAT", "BEAM RIDER","CTTN","RADAR","NOISE","SIGNAL","JAMMING"]
-
-cyber = ["FORWARD LOOKUP", "REQUEST", "ALLIGATOR", "NETWORK", "TRAFFIC"]
-
-civilian = [
-    "CIVILIAN", "CIV", "NON-COMBATANT", "NON-HOSTILE", "HUMANITARIAN", "REFUGEE", "CIVILIAN-AREA", "HUMANITARIAN-MISSION", "COMMERCIAL", "COMMERCIAL-VESSEL", "COMMERCIAL-PLANE", "COMMERCIAL A/C"
-]
-enem = ["ENEMY", "HOSTILE", "THREAT", "TARGET","ADVERSARY", "OPFOR", "OPPOSITION", "FOE", "RIVAL", "AGGRESSOR", "ANTAGONIST", "BANDIT", "BOGEY"]
-
-rando = ["RANDOM", "UNKNOWN", "UNK", "UNIDENTIFIED", "UNCONFIRMED", "UNCERTAIN", "POTENTIAL"]
 out =["SWIM","BEER"]
 
 battle_effect_dict = {
@@ -100,30 +37,20 @@ battle_effect_dict = {
 #     description = ' '.join(words[index+1:index+max_words]) if index + max_words <= len(words) else ' '.join(words[index+1:])
 
 def action_prompt(entity, description=""):
+    """
+    Check and see if the entity passed can be found in the "WORDS" lists, if so then retrieve the associated "ACTIONS" and return them
+    TO-DO: Review why is there a description parameter that we don't need
+    """
     if description:
         print(f"  Description: {description}")
 
-    for i in entity.split():
-        if i in air_enemy:
-            actions = ["Attack", "Investigate", "Communicate", "Destroy"]
-        elif i in Incoming:
-            actions = ["Counter", "Evade", "Brace for impact"]
-        elif i in surface:
-            actions = ["Attack", "Investigate", "Communicate"]
-        elif i in intel:
-            actions = ["Jam", "Communicate", "Investigate"]
-        elif i in cyber:
-            actions = ["Jam", "Hack", "Counter"]
-        elif i in civilian:
-            actions = ["Monitor", "Investigate", "Communicate"]
-        elif i in rando:
-            actions = ["Monitor", "Investigate", "Communicate"]
-        elif i in enem:
-            actions = ["Attack","Psyop","Harass"]
-        else:
-            actions = None, None, None
-        break
-    return actions
+    i = entity.split()[0]
+    for k in WORDS.keys():
+        if i in WORDS[k]:
+            return ACTIONS[k]
+    
+    return None, None, None
+
 
 def get_description(words, index, max_words=4):
     description = ""
@@ -172,7 +99,7 @@ def extracted_chat(message):
         filtered_word = ''.join(e for e in word if e.isalnum() or e == '-').upper()
         filtered_s = filtered_word.rstrip('S')
 
-        if filtered_s in air_enemy:
+        if filtered_s in WORDS["AIR"]:
             found_air.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -180,7 +107,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
 
-        if filtered_s in Incoming:
+        if filtered_s in WORDS["INCOMING"]:
             found_defend.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -188,7 +115,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
 
-        elif filtered_s in surface:
+        elif filtered_s in WORDS["SURFACE"]:
             found_surface.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -196,7 +123,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
 
-        elif filtered_s in intel:
+        elif filtered_s in WORDS["INTEL"]:
             found_intel.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -204,7 +131,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
 
-        elif filtered_s in cyber:
+        elif filtered_s in WORDS["CYBER"]:
             found_cyber.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -212,7 +139,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
 
-        elif filtered_s in civilian:
+        elif filtered_s in WORDS["CIVILIAN"]:
             found_civilian.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -220,7 +147,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
         
-        elif filtered_s in rando:
+        elif filtered_s in WORDS["RANDOM"]:
             found_rando.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
@@ -228,7 +155,7 @@ def extracted_chat(message):
             action1, action2, action3 = actions[:3]
             return filtered_s, action1, action2, action3
         
-        elif filtered_s in enem:
+        elif filtered_s in WORDS["ENEMIES"]:
             found_enemy.append(filtered_s)
             description = get_description(words, i)
             filtered_s = filtered_s + " " + description
